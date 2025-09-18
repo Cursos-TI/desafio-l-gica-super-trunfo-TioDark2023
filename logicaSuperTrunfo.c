9797c6c90642458ff1e9f5d097f5f4364aaeae8b
TioDark2023 <wheslley.lucas@hotmail.com>
2025-09-18 17:16:29 +0000

    Adicionei comparacao entre 2 atributos

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 
-int main(){
-
+int main(){ 
     // menu 
     int opcao,opcompara;
 
     printf("    <<<Menu Principal>>>     \n");
-    printf("1. ver Todas as cartas      \n");
-    printf("2. Ver comparacao completa  \n");
-    printf("3. comparacao detalhada     \n");
-    printf("4.       Sair               \n");
+    printf("1. Ver Todas as cartas       \n");
+    printf("2. Ver comparacao completa   \n");
+    printf("3. Comparacao detalhada      \n");
+    printf("4. Comparacao de 2 atributos \n");
+    printf("5.      SAIR                 \n");
     printf("Digite O Numero Para Seguir:"),(scanf("%d",&opcao)),printf("\n");
 
-    // Primeira carta
+    // Primeira carta 
     int carta=1,Populacao=7.330483,NumeroTuristicos=86;
     char Estado='G',Codigarta[10]="G01",NomeCidade[20]="Goiais";
     float Areakm=340.086;
@@ -46,6 +46,11 @@ int main(){
     poder2=pop2+notu2+akm2+pib2+(den2/1)+pibperc2;  // Soma total dos float 
    // Fim do segundo cod 
 
+    int atributo1, atributo2;
+    int valor1c1,valor2c1;
+    int soma1,soma2;
+    int Pow,pow2;
+  
  switch (opcao)
     { 
       case 1:
@@ -82,7 +87,6 @@ int main(){
     break;
     
     case 2:
-    
     printf("\n\n\n  Comparacao das cartas\n\n");
    if (Populacao > Populacao2){
       printf("A População Da carta 1 ganhou\n");
@@ -120,19 +124,12 @@ if (Areakm > Areakm2){
       printf("O Poder Da carta 2 ganhou\n");
    }
    break;
-   // fim cod
-    case 3:
-    printf ("       Escolha A Opcao   \n \n");
-    printf ("1. Populacao            \n");
-    printf ("2. Area em km           \n");
-    printf ("3. Pib                  \n");
-    printf ("4. Numeros Turisticos    \n");
-    printf ("5. Densidade Populacional \n");
-    printf ("6. Pib Percapita        \n");
-    printf ("7. Poder                \n");
-    printf ("8. Sair Para o Menu Principal:\n");
+   
+   case 3:
+    printf ("  Escolha A Opcao\n1. Populacao \n2.Area em km\n3.Pib\n4.Numeros Turisticos\n5.Densidade Populacional\n6.Pib Percapita\n7.Poder\n");
+    printf ("8.Sair Para o Menu Principal:\n");
     printf("Digite O Numero Para Seguir:"),scanf("%d",&opcompara),printf("\n");
-    switch (opcompara)
+   switch (opcompara)
 {
     case 1:
     printf(" O Maior Sempre Vence \n");
@@ -154,7 +151,8 @@ if (Areakm > Areakm2){
    }else{
       printf ("Houve Um Empate");
    }
-    break;
+   break;
+    
     case 3:
     printf("O Maior Sempre Vence \n");
     if (pib>pib2){
@@ -165,6 +163,7 @@ if (Areakm > Areakm2){
       printf ("Houve Um Empate");
    }
     break;
+   
     case 4:
     printf("O Maior Sempre Vence \n");
     if (NumeroTuristicos>NumeroTuristicos2){
@@ -174,7 +173,8 @@ if (Areakm > Areakm2){
    }else{
     printf ("Houve Um Empate");
    }
-    break;
+    break; 
+
     case 5:
     printf("O Maior Sempre Vence \n");
     if (Denspo>Denspo2){
@@ -185,6 +185,7 @@ if (Areakm > Areakm2){
      printf ("Houve Um Empate");
    }
     break;
+
     case 6:
     printf("O Maior Sempre Vence \n");
     if (pibperc>pibperc2){
@@ -195,6 +196,7 @@ if (Areakm > Areakm2){
      printf ("Houve Um Empate");
     }
     break;
+    
     case 7:
     printf("O Menor Sempre Vence \n");
     if (poder<poder2){
@@ -204,28 +206,100 @@ if (Areakm > Areakm2){
    }else{
     printf ("Houve Um Empate");
    }
-    break;
+   break;
 
     case 8:
     printf("voltando ao Menu \n");
-    return 0;
     break;
 
     default:
     printf("Opcao Nao Esta certa \n");
-    return 0;
     break;
-} // fim do cod 3 
-    case 4:
-    return 0;
-    break;
+    
+} 
+ break;   
+   case 4:
+    // Comparação de 2 atributos
+    printf("Primeiro Atributo Da Cidade\n\n1.População\n2.Área\n3.PIB\n4.Turísticos\n5.Densidade\n6.PIBper Capita\n7.poder\nEscolha o primeiro atributo da cidade:"),scanf("%d",&atributo1);
+    if (atributo1 < 1 || atributo1 > 7) {
+    printf("Número inválido! Escolha entre 1 e 7.\n");
+    break; 
+}
+    printf("Escolha o segundo Atributo: (diferente do primeiro):\n"),scanf("%d",&atributo2);
+    if (atributo2 < 1 || atributo2 > 7) {
+    printf("Número inválido! Escolha entre 1 e 7.\n");
+    break; 
+}
 
-    default:
-    printf("Opcao Nao Esta certa");
-    return 0;
-    break;
+   if (atributo1 == atributo2){
+   printf("Você não pode escolher o mesmo atributo duas vezes!\n");
+   break;
+          }
 
-    return 0;
+          // Pegar valores do atributo 1
+          switch (atributo1){
+              case 1: valor1c1=Populacao,printf("Primeiro atributo: População\n");                   break;
+              case 2: valor1c1=Areakm,printf("Primeiro atributo: Área (km²)\n");                     break;
+              case 3: valor1c1=Pib,printf("Primeiro atributo: PIB\n");                               break;
+              case 4: valor1c1=NumeroTuristicos,printf("Primeiro atributo: Pontos Turísticos\n");    break;
+              case 5: valor1c1=Denspo,printf("Primeiro atributo: Densidade Populacional\n");         break;
+              case 6: valor1c1=pibper,printf("Primeiro atributo: PIB per Capita\n");                 break;
+              case 7: valor1c1=poder,printf("Primeiro atributo: Poder\n");                           break; 
+              default: printf("Numero Errado, Por favor Escolha Outro\n");                             break;
+          }
+          // Pegar valores do atributo 2
+          switch (atributo2){
+              case 1: valor2c1=Populacao2,printf("\nSegundo atributo: População\n");                    break;
+              case 2: valor2c1=Areakm2,printf("\nSegundo atributo: Área (km²)\n");                      break;
+              case 3: valor2c1=Pib2,printf("\nSegundo atributo: PIB\n");                                break;
+              case 4: valor2c1=NumeroTuristicos2,printf("\nSegundo atributo: Pontos Turísticos\n");     break;
+              case 5: valor2c1=Denspo2,printf("\nSegundo atributo: Densidade Populacional\n");          break;
+              case 6: valor2c1=pibperc2,printf("\nSegundo atributo: PIB per Capita\n");                 break;
+              case 7: valor2c1=poder2,printf("\nSegundo atributo: Poder\n (Lembrando Tem Que Ter Menos que ele para ganhar* )\n");  break;
+              default: printf("Numero Errado, Por favor Escolha Outro\n");break;
+          }
+          
 
- }
+          
+// Comparação
+int pontos1 = 0, pontos2 = 0;   
+
+if (atributo1 == 7 && atributo2 != 7) {  
+    if (valor1c1 < valor2c1) 
+        pontos1++;
+    else 
+        pontos1--;  
+}
+else if (atributo2 == 7 && atributo1 != 7) {
+    if (valor2c1 < valor1c1) 
+    pontos2--;  
+    else 
+    pontos2++;  
+}
+else {
+    if (valor1c1 < valor2c1) pontos1++;
+    if (valor2c1 > valor1c1) pontos1++;
+    if (soma1 > soma2) pontos1++;
+    else if (soma2 > soma1) pontos2++;
+}
+
+if (pontos1 > pontos2) {
+    printf("\n%s venceu!\n", NomeCidade);
+    printf("Valores finais:\n");
+    printf("Atributo Da Carta 1:( %d ) vs Atributo Da Carta 2: ( %d )", valor1c1, valor2c1);
+
+}
+else if (pontos2 > pontos1) {
+    printf("\n%s venceu!\n", NomeCidade2);
+    printf("Valores finais:\n");
+    printf("Atributo Da Carta 2: ( %d ) vs Atributo Da Carta 1: ( %d )", valor2c1, valor1c1);
+}
+else {
+    printf("\nEmpate!\n");
+    printf("Valores finais:\n");
+    printf("Atributo Da Carta 1: ( %d ) vs Atributo Da Carta 2: ( %d  )", valor1c1, valor2c1);
+
+}
+return 0;
+}
 }
